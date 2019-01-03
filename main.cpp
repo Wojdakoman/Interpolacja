7e3e61f06e3ff6314fab1373a5ec2d8d03306303
Wojdakoman <logiblogi@gmail.com>
2019-01-03 21:34:52 +0100

    Poprawa
    Poprawa testu, dodanie paraboli x^2+4x+4, zmiana sinusa na 0 - pi

--- main.cpp
+++ main.cpp
@@ -6,14 +6,16 @@ double lag(int, double[], int, double);
 double wiel(double[], double[], int, double);
 
 void forSin();
+void parabola();
 
 int main() {
-	/*double tabx[3] = { 1,4,9 };
-	double taby[3] = { 1,2,3 };
+	/*double taby[3] = { 1,4,9 };
+	double tabx[3] = { 1,2,3 };
 
 	cout << "Suma "<<wiel(tabx, taby, 3, 7)<<endl;*/
 
 	forSin();
+	//parabola();
 
 	//system("pause");
 }
@@ -46,12 +48,33 @@ void forSin() {
 	ofstream plik;
 	plik.open("dane.txt");
 
-	double xs[] = {0,0.5236,0.7854,1.0472,1.5708};
-	double ys[] = { 0,0.5,0.7071,0.866,1 };
+	double xs[] = {
+		0,
+		0.52359877559829887307710723054658,
+		0.78539816339744830961566084581988,
+		1.0471975511965977461542144610932,
+		1.5707963267948966192313216916398,
+		3.1415926535897932384626433832795
+	};
+	double ys[] = {
+		0,
+		0.5,
+		0.70710678118654752440084436210485,
+		0.86602540378443864676372317075294,
+		1,0
+	};
 	//n=5
 
-	for (double i = -1.57; i <= 2*3.14; i += 0.01) {
-		plik << i << ";" << wiel(xs, ys, 5, i) << endl;
+	for (double i = 0; i <= 3.142; i += 0.001) {
+		plik << i << ";" << wiel(xs, ys, 6, i) << endl;
 	}
 	plik.close();
+}
+
+void parabola() {
+	double tabx[] = { -2, 0 ,-1,-3};
+	double taby[] = { 0, 4,1,1};
+
+	cout << wiel(tabx, taby, 4, 0.647) << endl; //16: x=-6 x=2
+
 }
